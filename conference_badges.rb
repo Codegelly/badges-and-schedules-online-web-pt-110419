f525bdfc77d6b1d869532b8a7bbea4b350f0c508
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-08 00:15:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,8 @@ end
   
   def batch_badge_creator(name)
     array = []
-    array.each do |name|
-     # array << "Hello, my name is #{name}."
+    name.each do |name|
+     array << "Hello, my name is #{name}."
   end 
   return  "Hello, my name is #{name}."
 end 
